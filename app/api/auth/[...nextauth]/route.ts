35b387d76183b1525fff2bf77517b64b725237ff
wexcrai <ilyastekkan@gmail.com>
2026-05-24 20:05:32 +0300

    fix: credentials type hatası düzeltildi

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,7 @@ const handler = NextAuth({
           credentials?.username === process.env.ADMIN_USERNAME &&
           credentials?.password === process.env.ADMIN_PASSWORD
         ) {
-          return { id: "1", name: credentials.username };
+         return { id: "1", name: credentials?.username ?? "" };
         }
         return null;
       },
